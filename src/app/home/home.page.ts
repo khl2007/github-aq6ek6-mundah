f85db796ee647b2829ce23c54b96e2d7437b04ad
khl2007 <khalilqandil@gmail.com>
2020-04-05 22:54:07 -0700

    3 files modified

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,31 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../services/auth.service';
-import { LoadingController } from '@ionic/angular';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import { LoadingController } from "@ionic/angular";
+import { Router, ActivatedRoute } from "@angular/router";
 
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 
-import { map } from 'rxjs/operators';
+import { map } from "rxjs/operators";
 
-import { FirebaseService } from '../services/firebase.service';
-import { Blogitem } from '../services/blogitem';
+import { FirebaseService } from "../services/firebase.service";
+import { Blogitem } from "../services/blogitem";
 
-import { User } from '../services/user';
+import { User } from "../services/user";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  selector: "app-home",
+  templateUrl: "./home.page.html",
+  styleUrls: ["./home.page.scss"]
 })
 export class HomePage implements OnInit {
-
   items: Array<any>;
 
-blogs: any;
-crtuser: Observable<User>;
-public like_btn = {
-    color: 'black',
-    icon_name: 'heart-outline'
+  blogs: any;
+  crtuser: Observable<User>;
+  public like_btn = {
+    color: "black",
+    icon_name: "heart-outline"
   };
 
   public tap: number = 0;
@@ -36,47 +35,44 @@ public like_btn = {
     private router: Router,
     private route: ActivatedRoute,
     private firebaseService: FirebaseService
-  ) { }
+  ) {}
 
   ngOnInit() {
     if (this.route && this.route.data) {
       this.getData();
     }
     this.getBlogs();
-this.getCrtusertest();
-
+    this.getCrtusertest();
   }
 
-  async getData(){
+  async getData() {
     const loading = await this.loadingCtrl.create({
-      message: 'Please wait...'
+      message: "Please wait..."
     });
     this.presentLoading(loading);
 
     this.route.data.subscribe(routeData => {
-      routeData['data'].subscribe(data => {
+      routeData["data"].subscribe(data => {
         loading.dismiss();
         this.items = data;
-      })
-    })
+      });
+    });
   }
-getCrtusertest(){
-
- this.crtuser= this.firebaseService.getUserInfo().valueChanges();
-
- 
-
-}
-getBlogs() {
-    this.firebaseService.getBlogs().snapshotChanges().pipe(
-      map(changes =>
-        changes.map(c =>
-          ({ key: c.payload.doc.id, ...c.payload.doc.data() })
+  getCrtusertest() {
+    this.crtuser = this.firebaseService.getUserInfo().valueChanges();
+  }
+  getBlogs() {
+    this.firebaseService
+      .getBlogs()
+      .snapshotChanges()
+      .pipe(
+        map(changes =>
+          changes.map(c => ({ key: c.payload.doc.id, ...c.payload.doc.data() }))
         )
       )
-    ).subscribe(blogs => {
-      this.blogs = blogs;
-    });
+      .subscribe(blogs => {
+        this.blogs = blogs;
+      });
   }
 
   async presentLoading(loading) {
@@ -84,48 +80,48 @@ getBlogs() {
   }
 
   likeButton() {
-    if(this.like_btn.icon_name === 'heart-outline') {
-      this.like_btn.icon_name = 'heart';
-      this.like_btn.color = 'danger';
+    if (this.like_btn.icon_name === "heart-outline") {
+      this.like_btn.icon_name = "heart";
+      this.like_btn.color = "danger";
       // Do some API job in here for real!
-    }
-    else {
-      this.like_btn.icon_name = 'heart-outline';
-      this.like_btn.color = 'black';
+    } else {
+      this.like_btn.icon_name = "heart-outline";
+      this.like_btn.color = "black";
     }
   }
 
-  tapPhotoLike(times) { // If we click double times, it will trigger like the post
+  tapPhotoLike(times) {
+    // If we click double times, it will trigger like the post
     this.tap++;
-    if(this.tap % 2 === 0) {
+    if (this.tap % 2 === 0) {
       this.likeButton();
     }
   }
 
-swipePage(event) {
-    if(event.direction === 1) { // Swipe Left
+  swipePage(event) {
+    if (event.direction === 1) {
+      // Swipe Left
       console.log("Swap Camera");
-    } 
+    }
 
-    if(event.direction === 2) { // Swipe Right
+    if (event.direction === 2) {
+      // Swipe Right
       this.goMessages();
     }
-    
   }
 
   scrollToTop() {
     this.content.scrollToTop();
   }
 
-
-
-  logout(){
-    this.authService.doLogout()
-    .then(res => {
-      this.router.navigate(["/login"]);
-    }, err => {
-      console.log(err);
-    })
+  logout() {
+    this.authService.doLogout().then(
+      res => {
+        this.router.navigate(["/login"]);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
-
 }
