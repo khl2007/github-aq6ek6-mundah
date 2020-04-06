3c8d1df3b0ed2a33f05039f3d79086737b1c03c5
khl2007 <khalilqandil@gmail.com>
2020-04-06 23:30:03 +0300

    Update firebase.service.ts

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -47,7 +47,7 @@ return this.afs.collection('/blogs');
 getUserInfoo(){
 let thetestuser: any;
 let currentUser = firebase.auth().currentUser;
-this.afs.doc<User>('users/' + currentUser.uid).valueChanges().subscripe(data => thetestuser = data);
+this.afs.doc<User>('users/' + currentUser.uid).valueChanges().subscribe(data => thetestuser = data);
 return thetestuser;
 }
 
