df7fe7592aa8f29cc8e3df1a0b9535f34ae9e705
khl2007 <khalilqandil@gmail.com>
2020-04-10 01:43:29 +0300

    Update publicprofile.page.ts

--- src/app/publicprofile/publicprofile.page.ts
+++ src/app/publicprofile/publicprofile.page.ts
@@ -25,6 +25,14 @@ userData: User = new User();
 
 blogPost: Blogitem[] = [];
 
+public like_btn = {
+    color: "black",
+    icon_name: "heart-outline"
+  };
+
+  public tap: number = 0;
+
+
   constructor(private route: ActivatedRoute,private router: Router , private firebaseService: FirebaseService) { }
 
   ngOnInit() {
@@ -65,5 +73,39 @@ console.log(this.blogPost);
 
   }
 
+  likeButton() {
+    if (this.like_btn.icon_name === "heart-outline") {
+      this.like_btn.icon_name = "heart";
+      this.like_btn.color = "danger";
+      // Do some API job in here for real!
+    } else {
+      this.like_btn.icon_name = "heart-outline";
+      this.like_btn.color = "black";
+    }
+  }
+
+  tapPhotoLike(times) {
+    // If we click double times, it will trigger like the post
+    this.tap++;
+    if (this.tap % 2 === 0) {
+      this.likeButton();
+    }
+  }
+
+  swipePage(event) {
+    if (event.direction === 1) {
+      // Swipe Left
+      console.log("Swap Camera");
+    }
+
+    if (event.direction === 2) {
+      // Swipe Right
+      this.goMessages();
+    }
+  }
+
+  scrollToTop() {
+    this.content.scrollToTop();
+  }
 
 }
