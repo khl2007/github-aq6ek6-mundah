237a8c0aebc6f92680c75f6b983add926511451b
khl2007 <khalilqandil@gmail.com>
2020-04-10 10:11:29 -0700

    3 files modified

--- src/app/chats/chats.page.ts
+++ src/app/chats/chats.page.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewChild } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import { LoadingController } from "@ionic/angular";
+import { Router, ActivatedRoute } from "@angular/router";
+
+import { Animation, AnimationController } from '@ionic/angular';
+
+import { IonInfiniteScroll } from '@ionic/angular';
+
+import { Observable } from "rxjs";
+
+import { map } from "rxjs/operators";
+
+import { FirebaseService } from "../services/firebase.service";
+import { Blogitem } from "../services/blogitem";
+
+import { User } from "../services/user";
 
 @Component({
   selector: 'app-chats',
@@ -7,7 +23,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatsPage implements OnInit {
 
-  constructor() { }
+  constructor(private firebaseService: FirebaseService) { }
 
   ngOnInit() {
   }
