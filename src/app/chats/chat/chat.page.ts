4b986d88d071e2dcf9b792c186de2bc5125165c1
khl2007 <khalilqandil@gmail.com>
2020-04-11 00:36:21 +0300

    Create chat.page.ts

---
+++ src/app/chats/chat/chat.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit , ViewChild } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import { LoadingController } from "@ionic/angular";
+import { Router, ActivatedRoute } from "@angular/router";
+
+import { Animation, AnimationController } from '@ionic/angular';
+
+import { IonInfiniteScroll } from '@ionic/angular';
+
+import { Observable } from "rxjs";
+
+import { map } from "rxjs/operators";
+
+import { FirebaseService } from "../services/firebase.service";
+import { Blogitem } from "../services/blogitem";
+
+import { User } from "../services/user";
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.page.html',
+  styleUrls: ['./chat.page.scss'],
+})
+export class ChatPage implements OnInit {
+
+chats: any;
+
+  constructor(public loadingCtrl: LoadingController,
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private firebaseService: FirebaseService,
+    private animationCtrl: AnimationController) { }
+
+  ngOnInit() {
+
+    this.firebaseService.getChats().subscribe(res => {
+      console.log(res);
+    this.chats = res;
+    }); 
+  }
+
+ 
+}
